a10d8eceb36a34d56b0287823cbac9a808d3bcb5
inishantxchandel <nishantxchandel@gmail.com>
2023-10-28 15:51:14 +0530

    fixed pg error

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { Sequelize } = require('sequelize');
+import pg from 'pg';
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
@@ -8,6 +9,8 @@ const sequelize = new Sequelize({
   username: 'healthtrip_telegram_bot',
   password: 'teBXxD9EVHW6x9NG5h5cPtjEok9cFj2z',
   host: 'dpg-ckt29h8168ec73bv7n6g-a.oregon-postgres.render.com',
+  dialectModule: pg, // I've added this.
+
   port: 5432, // Change the port if necessary
   ssl: true, // Enable SSL/TLS
   dialectOptions: {
